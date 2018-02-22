476a1f88bc9ddc31779bbcd0d8e059ac7e721197
Christian Schuhmann <madebyherzblut@users.noreply.github.com>
2018-02-22 22:02:32 +0100

    Add handoff page

---
+++ static/js/main.js
@@ -0,0 +1,34 @@
+jQuery(function($) {
+    if ($("#map").length) {
+        var map = L.map("map").setView(
+            [52.51371369804256, 13.42460632324219],
+            10
+        );
+
+        L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+            attribution:
+                '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        // map.on('moveend', function () {
+        //   console.log(map.getCenter());
+        // });
+
+        navigator.geolocation.getCurrentPosition(function(position) {
+            jQuery(".logo--small").removeClass("loading");
+
+            var currentPosition = [
+                position.coords.latitude,
+                position.coords.longitude
+            ];
+
+            map.setView(currentPosition);
+
+            L.marker(currentPosition).addTo(map);
+        });
+    }
+
+    $('button[type="submit"]').click(function() {
+        $('.logo').addClass('loading');
+    });
+});
